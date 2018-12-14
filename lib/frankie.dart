34d9ce9cd4daa3840c9365afada4803082fbcf7d
claudiopaolicelli <claudio.paolicelli@endclothing.com>
2018-12-14 11:36:27 +0000

    Added removed useless comments

--- lib/frankie.dart
+++ lib/frankie.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Frankie extends StatelessWidget {
 
@@ -8,19 +9,36 @@ class Frankie extends StatelessWidget {
     return GestureDetector(
       // move frankie to right
       onVerticalDragStart: (dragStartDetails) {
-
+        Fluttertoast.showToast(
+            msg: dragStartDetails.toString(),
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.TOP,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white
+        );
       },
       //move frankie to left
       onVerticalDragEnd: (dragEndDetails) {
-
+        Fluttertoast.showToast(
+            msg: dragEndDetails.toString(),
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.TOP,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white
+        );
       },
       //frankie image
-      child: new Image.asset(
-          "assets/images/car-top-view.png",
-          width: 400.0,
-          height: 100.0,
-          alignment: new Alignment(-1.0, -1.0)
-      ),
+      child: new Column(
+          children: <Widget>[
+            new Image.asset(
+                "assets/images/car-top-view.png",
+                width: 400.0,
+                height: 100.0
+            )
+          ]
+      )
     );
   }
 
